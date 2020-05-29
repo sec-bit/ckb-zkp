cb16ffea9981bf407b41ff755f16d8f70f38660a
sun <huachuang20@gmail.com>
2020-05-29 23:25:14 +0800

    refs #1 fix variable name

--- src/groth16.rs
+++ src/groth16.rs
@@ -20,34 +20,42 @@ pub fn groth16_verify<E: PairingEngine>(bytes: &[u8]) -> bool {
 pub struct Groth16Proof<E: PairingEngine> {
     pvk: PreparedVerifyingKey<E>,
     proof: Proof<E>,
-    vk: Vec<E::Fr>,
+    public_inputs: Vec<E::Fr>,
 }
 
 impl<E: PairingEngine> Groth16Proof<E> {
-    pub fn new(pvk: PreparedVerifyingKey<E>, proof: Proof<E>, vk: Vec<E::Fr>) -> Self {
-        Self { pvk, proof, vk }
+    pub fn new(pvk: PreparedVerifyingKey<E>, proof: Proof<E>, public_inputs: Vec<E::Fr>) -> Self {
+        Self {
+            pvk,
+            proof,
+            public_inputs,
+        }
     }
 
     pub fn from_bytes(mut bytes: &[u8]) -> Result<Groth16Proof<E>, ()> {
         let vk = VerifyingKey::<E>::read(&mut bytes).unwrap();
         let pvk = prepare_verifying_key(&vk);
         let proof = Proof::<E>::read(&mut bytes).unwrap();
-        let vk_len = u64::read(&mut bytes).unwrap();
-        let mut vk = vec![];
-        for _ in 0..vk_len {
-            let v = <E::Fr>::read(&mut bytes).unwrap();
-            vk.push(v);
+        let public_inputs_len = u64::read(&mut bytes).unwrap();
+        let mut public_inputs = vec![];
+        for _ in 0..public_inputs_len {
+            let p = <E::Fr>::read(&mut bytes).unwrap();
+            public_inputs.push(p);
         }
 
-        Ok(Self { pvk, proof, vk })
+        Ok(Self {
+            pvk,
+            proof,
+            public_inputs,
+        })
     }
 
     pub fn to_bytes(&self, vk: &VerifyingKey<E>) -> Vec<u8> {
         let mut bytes = vec![];
         vk.write(&mut bytes).unwrap();
         self.proof.write(&mut bytes).unwrap();
-        (self.vk.len() as u64).write(&mut bytes).unwrap();
-        for i in &self.vk {
+        (self.public_inputs.len() as u64).write(&mut bytes).unwrap();
+        for i in &self.public_inputs {
             i.write(&mut bytes).unwrap();
         }
 
@@ -55,6 +63,6 @@ impl<E: PairingEngine> Groth16Proof<E> {
     }
 
     pub fn destruct(self) -> (PreparedVerifyingKey<E>, Proof<E>, Vec<E::Fr>) {
-        (self.pvk, self.proof, self.vk)
+        (self.pvk, self.proof, self.public_inputs)
     }
 }
