f9cbed3b0ec0b7991de278ab3cb7e077069c1aa7
lujw <lu.jw@outlook.com>
2020-06-09 17:07:44 +0800

    Update contract to meet new zkp-toolkit.
    1. Use script args and output data to load vk file and proof file.
    2. update dependency ckb-std to 0.3.0.

--- contracts/ckb-zkp/src/main.rs
+++ contracts/ckb-zkp/src/main.rs
@@ -4,32 +4,61 @@
 #![feature(alloc_error_handler)]
 #![feature(panic_info_message)]
 
+use alloc::vec::Vec;
 use ckb_std::{ckb_constants, debug, default_alloc, entry, error::SysError, syscalls};
+use ckb_types::{packed::*, prelude::*};
 use zkp::verify_from_bytes;
 
 #[no_mangle]
 pub fn main() -> i8 {
+    debug!("Started.");
     let mut index = 0;
-    const LEN: usize = 336; // MIMC groth16 bn256 size
-    let offset = 0;
+    // Proof file size:
+    // MIMC groth16 bn256: 299 B
+    // MIMC groth16 bls12_381: 427 B
+    const DATA_LEN: usize = 427;
+    // Vk file size:
+    // MIMC groth16 bn256: 590 B
+    // MIMC groth16 bls12_381: 878 B
+    const ARG_LEN: usize = 878;
+    const OFFSET: usize = 0;
 
     loop {
-        let mut buffer = [0u8; LEN];
+        let mut data_buffer = [0u8; DATA_LEN];
+        let mut arg_buffer = [0u8; ARG_LEN];
+        // read proof from output data field.
         match syscalls::load_cell_data(
-            &mut buffer,
-            offset,
+            &mut data_buffer,
+            OFFSET,
             index,
             ckb_constants::Source::GroupOutput,
         ) {
-            Ok(_) => {
-                debug!("idx: {}, data length: {}", index, buffer.len());
-                // debug!("{:?}", buffer);
-                if verify_from_bytes(&buffer) {
-                    debug!("Verification succeeded.");
-                // return 0;
-                } else {
-                    debug!("Verification failed.");
-                    return -1;
+            Ok(data_len_result) => {
+                // read vk file from script arg field.
+                match syscalls::load_cell_by_field(
+                    &mut arg_buffer,
+                    OFFSET,
+                    index,
+                    ckb_constants::Source::GroupOutput,
+                    ckb_constants::CellField::Type,
+                ) {
+                    Ok(arg_len_result) => {
+                        let type_script =
+                            Script::new_unchecked(arg_buffer[..arg_len_result].into());
+                        let args: Vec<u8> = type_script.args().unpack();
+
+                        // execute the verification.
+                        if verify_from_bytes(&data_buffer[0..data_len_result], &args) {
+                            debug!("Verification succeeded.");
+                        // return 0;
+                        } else {
+                            debug!("Verification failed.");
+                            return -1;
+                        }
+                    }
+                    Err(err) => {
+                        debug!("{:?}", err);
+                    }
                 }
             }
             Err(err) => {
@@ -45,4 +74,4 @@ pub fn main() -> i8 {
 }
 
 entry!(main);
-default_alloc!(256 * 1024, 16);
+default_alloc!(4 * 1024, 64 * 1024, 64);
