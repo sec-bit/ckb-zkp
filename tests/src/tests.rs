d5242dcb1ade4751a6c83687e1193395b108bd64
sun <huachuang20@gmail.com>
2020-06-29 12:01:13 +0800

    add mimc bulletproofs verifier

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -9,8 +9,9 @@ use ckb_tool::ckb_types::{
 use std::fs::File;
 use std::io::Read;
 
-const CONTRACT_NAME: &str = "mimc-groth16-verifier";
-const MAX_CYCLES: u64 = 10000_000_000;
+//const CONTRACT_NAME: &str = "mimc-groth16-verifier";
+const CONTRACT_NAME: &str = "mimc-bulletproofs-verifier";
+const MAX_CYCLES: u64 = 1_000_000_000_000;
 
 // Relative path starts from capsuled-contracts/tests.
 const VK_DIR: &str = "../cli/trusted_setup";
@@ -22,16 +23,25 @@ const PROOF_BN_256: &str = "mimc.groth16-bn_256.proof";
 const VK_BLS12_381: &str = "mimc-groth16-bls12_381.vk";
 const PROOF_BLS12_381: &str = "mimc.groth16-bls12_381.proof";
 
-#[test]
+//const BULLETPROOFS_BN_256: &str = "mimc.bulletproofs-bn_256.proof";
+const BULLETPROOFS_BN_256: &str = "greater.bulletproofs-bn_256.proof";
+
+//#[test]
 // #[ignore]
 fn test_proof_bn_256() {
     proving_test(VK_BN_256, PROOF_BN_256);
 }
 
-#[test]
+//#[test]
 // #[ignore]
 fn test_proof_bls12_381() {
     proving_test(VK_BLS12_381, PROOF_BLS12_381);
+}
+
+#[test]
+// #[ignore]
+fn test_bulletproofs_bn_256() {
+    proving_test(VK_BN_256, BULLETPROOFS_BN_256);
 }
 
 #[test]
@@ -111,6 +121,7 @@ fn proving_test(vk_file: &str, proof_file: &str) {
     proof_file
         .read_to_end(&mut proof_bin)
         .expect("Failed to read proof file");
+    println!("proof file size: {}", proof_bin.len());
 
     let mut vk_file = File::open(format!("{}/{}", VK_DIR, vk_file)).expect("VK file not exists");
     let mut vk_bin = Vec::new();
