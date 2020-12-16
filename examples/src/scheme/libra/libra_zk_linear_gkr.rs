5226124db92368fca59ff7247c3e283d2ff72031
even <even@secbit.io>
2020-12-16 18:09:21 +0800

    examples: add annotation

--- examples/src/scheme/libra/libra_zk_linear_gkr.rs
+++ examples/src/scheme/libra/libra_zk_linear_gkr.rs
@@ -5,6 +5,16 @@ use scheme::libra::circuit::Circuit;
 use scheme::libra::libra_zk_linear_gkr::ZKLinearGKRProof;
 use scheme::libra::params::Parameters;
 
+/// circuit structure
+///layer3: gate0 = (add, 0, 1), gate1 = (add, 1, 2), gate2 = (mul, 2, 3), gate3 = (mul, 1, 3)
+///layer2: gate0 = (mul, 0, 1), gate1 = (add, 2, 3), gate2 = (add, 4, 5), gate3 = (mul, 6, 7)
+///layer1: gate0 = (mul, 0, 1), gate1 = (add, 2, 3), gate2 = (add, 4, 5), gate4 = (mul, 6, 7), gate5 = (mul, 15, 8), gate6 = (mul, 9, 10), gate7 =(add, 11, 12), gate8 =(add, 13, 14)
+///layer0: witness0,  witness1,  witness2,  witness3,  witness4,  witness5,  witness6,  witness7, input0, input1, input2, input3, input4, input5, input6, input7, input8
+///layer0 is input-layer, The first eight items are witnesses, and the last eight items are public inputs
+///layer1~layer3 are composed of multiple gates.
+///     gaten = (op, left, right) represents the nth gate, op represents the operator, add or multiple,
+///     left represents the number of its left node, right  represents the number of its right node
+///     e.g. layer2: gate0 = (mul, 0, 1) represents layer2-gate0 = layer1-gate0 * layer1-gate1
 fn prepare_construct_circuit<G: Curve>() -> (Vec<G::Fr>, Vec<G::Fr>, Vec<Vec<(u8, usize, usize)>>) {
     let mut inputs = Vec::new();
     let mut witnesses = Vec::new();
