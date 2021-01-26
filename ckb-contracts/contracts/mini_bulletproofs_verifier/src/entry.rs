ed8a8ceff611d957f0bde784eca77d429d64139d
sun <huachuang20@gmail.com>
2021-01-26 14:10:45 +0800

    fix default features

--- ckb-contracts/contracts/mini_bulletproofs_verifier/src/entry.rs
+++ ckb-contracts/contracts/mini_bulletproofs_verifier/src/entry.rs
@@ -5,22 +5,20 @@ use ckb_std::{ckb_constants::Source, high_level::load_cell_data};
 
 use crate::error::Error;
 
-use zkp_toolkit::{
-    bn_256::{Bn_256 as E, Fr},
-    bulletproofs::{verify_proof, Generators, Proof, R1csCircuit},
-    math::PrimeField,
-    r1cs::{ConstraintSynthesizer, ConstraintSystem, SynthesisError},
-};
+use ark_bls12_381::{Bls12_381 as E, Fr};
+use ark_serialize::*;
+use zkp_bulletproofs::{verify_proof, Generators, Proof, R1csCircuit};
+use zkp_r1cs::{ConstraintSynthesizer, ConstraintSystem, SynthesisError};
 
-struct Mini<F: PrimeField> {
-    pub x: Option<F>,
-    pub y: Option<F>,
-    pub z: Option<F>,
+struct Mini {
+    pub x: Option<Fr>,
+    pub y: Option<Fr>,
+    pub z: Option<Fr>,
     pub num: u32,
 }
 
-impl<F: PrimeField> ConstraintSynthesizer<F> for Mini<F> {
-    fn generate_constraints<CS: ConstraintSystem<F>>(
+impl ConstraintSynthesizer<Fr> for Mini {
+    fn generate_constraints<CS: ConstraintSystem<Fr>>(
         self,
         cs: &mut CS,
     ) -> Result<(), SynthesisError> {
@@ -37,7 +35,7 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for Mini<F> {
             cs.enforce(
                 || "x * (y + 2) = z",
                 |lc| lc + var_x,
-                |lc| lc + var_y + (F::from(2u32), CS::one()),
+                |lc| lc + var_y + (Fr::from(2u32), CS::one()),
                 |lc| lc + var_z,
             );
         }
@@ -65,12 +63,15 @@ pub fn main() -> Result<(), Error> {
         Err(err) => return Err(err.into()),
     };
 
-    let (gens, r1cs, proof): (Generators<E>, R1csCircuit<E>, Proof<E>) =
-        postcard::from_bytes(&proof_data).map_err(|_e| Error::Encoding)?;
-    let publics: Vec<Fr> = postcard::from_bytes(&public_data).map_err(|_e| Error::Encoding)?;
+    let gens = Generators::<E>::deserialize(&proof_data[..]).map_err(|_e| Error::Encoding)?;
+    let r1cs = R1csCircuit::<E>::deserialize(&proof_data[..]).map_err(|_e| Error::Encoding)?;
+    let proof = Proof::<E>::deserialize(&proof_data[..]).map_err(|_e| Error::Encoding)?;
+
+    let mut publics = Vec::new();
+    publics.push(Fr::deserialize(&public_data[..]).map_err(|_e| Error::Encoding)?);
 
     // Demo circuit
-    let _c = Mini::<Fr> {
+    let _c = Mini {
         x: None,
         y: None,
         z: None,
